2faa88c177bce7f0d786ac8ea3b9b316862aaae1
Rashmi Priya <rashmi.priya.3110@gmail.com>
2025-12-27 22:11:36 +0530

    Add files via upload

--- dataCleaner.py
+++ dataCleaner.py
@@ -12,10 +12,20 @@ def data_cleaner(file):
     df=df.replace('',np.nan)
     df=df.replace('nan',np.nan)
     df.replace(r'!@#$%^&\*',np.nan,regex=True)
+    print(df)
 
 file_path=input('Enter the path of the file you want to clean the data of:')
+df=pd.read_csv(file_path)
 file_path = file_path.strip(' "')
+
+
+columnToNum=input('Are there any columns with numbers taken as string. If yes, how many? Else, enter \'0\'')
+for i in columnToNum:
+    nameCol=input('Name of the column as it is:')
+    df[nameCol]=pd.to_numeric(df[columnToNum],errors='coerce')
+
+columnToDate=input('Are there any columns with dates and (or) times taken as string. If yes, how many? Else, enter \'0\'')
+for i in columnToDate:
+    nameCol=input('Name of the column as it is:')
+    df[nameCol]=pd.to_datetime(df[columnToNum],errors='coerce')
 data_cleaner(file_path)
-
-
-
